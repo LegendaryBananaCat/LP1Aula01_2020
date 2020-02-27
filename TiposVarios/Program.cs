91cdaecd113adf104af73a502e4cf71684dcc9c6
BananaCat <joana.f.silva.99@gmail.com>
2020-02-27 11:37:34 +0000

    Add Vars

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace TiposVarios
+{
+    class Program 
+    {
+        static int i = 53;
+        static double x = 3.553;
+        static decimal z = 3.1415m;
+        static uint j = 52U;
+        static long y = 5L;
+        static ulong n = 45UL;
+        static char r = 'y';
+        static string t = "yeetus commitus -m. /n yeeet";
+
+        static void Main (string[] args)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine(i);
+            Console.WriteLine(x);
+            Console.WriteLine(z);
+            Console.WriteLine(j);
+            Console.WriteLine(y);
+            Console.WriteLine(n);
+            Console.WriteLine(r);
+            Console.WriteLine(t);
+        }
+    }
+}
